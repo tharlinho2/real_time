167eb78c1139d3ded2d506c2437c8e3db950eb8f
Tharles Ribeiro Pereira <tharlinho2@gmail.com>
2023-02-26 00:24:14 -0300

    add deamonite

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -5,3 +5,6 @@ import * as bootstrap from "bootstrap"
 import "./channels"
 import "./packs/service_worker.js"
 // import "./packs/localization.js"
+import './packs/flash_messages.js'
+import "daemonite-material";
+import "jquery"
